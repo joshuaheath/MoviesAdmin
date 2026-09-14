ce480ed058ba0d5af0da61c437127b65f7d8e7ab
w0521056@campus.nscc.ca <W0521056@campus.nscc.ca>
2026-09-14 10:35:38 -0300

    Created attributes for MovieData model

---
+++ MoviesAdmin/Models/Friend.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Friend
+    {
+        public int      Id {get; set;} = 0;
+        public string   Name {get; set;} = string.Empty;
+        public string   Email {get; set;} = string.Empty;
+    }
+}
---
+++ MoviesAdmin/Models/MovieData.cs
@@ -0,0 +1,22 @@
+﻿namespace MoviesAdmin.Models
+{
+    // This class is in charge of data relating movies
+    // title
+    // synopsis
+    // genre
+    // rating (eg PG-13)
+    // runtimeMin (how long the movie will be)
+    // release data (done as a date time object)
+    public class MovieData
+    {
+        public string title { get; set; } = string.Empty;
+        public string synopsis { get; set; } = string.Empty;
+        
+        public string genre { get; set; } = string.Empty;
+        public string rating  { get; set; }
+
+        public int runtimeMin { get; set; }
+
+        public DateTime releaseDate { get; set; } = DateTime.Now;
+    }
+}
